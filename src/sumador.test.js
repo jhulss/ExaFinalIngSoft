8f17f9ef27018e82736bf9bd13c2a7da231b3589
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-06-01 09:43:45 -0400

    Primer test

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -1,7 +1,8 @@
-import sumar from "./sumador.js";
+import cambiar from "./sumador.js";
 
-describe("Sumar", () => {
-  it("deberia sumar dos numeros", () => {
-    expect(sumar(3, 2)).toEqual(5);
+describe("cambiar", () => {
+  it("deberia cambiar monedas simples en su unidad minima", () => {
+    expect(cambiar(1)).toEqual(1);
   });
+
 });
