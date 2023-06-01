2d705cec5b66c7c0e6d1abc712084b2a366bd96a
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-06-01 09:58:34 -0400

    Segundo ejercicio

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -12,5 +12,13 @@ describe("cambiar", () => {
   it("cuando recibe cortes distintos, los cambia por monedas de 1 Bs", () => {
     expect(cambiar(6)).toEqual("1,1,1,1,1,1");
   });
-  
+
+  it('Fracciona correctamente el monto de dinero', () => {
+    expect(fraccionarMonto(19)).toEqual([5, 5, 5, 2, 2]);
+    expect(fraccionarMonto(10)).toEqual([5, 5]);
+    expect(fraccionarMonto(3)).toEqual([2, 1]);
+    expect(fraccionarMonto(1)).toEqual([1]);
+  });
+
 });
+
